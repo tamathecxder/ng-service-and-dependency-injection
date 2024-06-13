844b0eb7bc25dc398c673c1c021c84e703a41fd2
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-06-13 18:44:13 +0700

    refactor app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { Account } from './shared/models/account.model';
+import { CommonModule } from '@angular/common';
+import { AccountFormComponent } from './account-form/account-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AccountsComponent, AccountFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-service-and-dependency-injection';
+
+  accounts: Account[] = [];
+
+  onaccountAdded(data: Account) {
+    this.accounts.push(data);
+  }
 }
